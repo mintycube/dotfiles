843019d67871ad3d02886b72d4e07ebf39fc1da6
mintycube <90507714+osama-aslam@users.noreply.github.com>
2023-10-10 18:50:18 +0500

    made fzf scripts more modular

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -29,31 +29,31 @@ return {
 			},
 		},
 	},
-
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-    },
-    event= "LspAttach",
-    config  = function ()
-      vim.opt.updatetime = 200
-
-      require("barbecue").setup({
-        create_autocmd = false,
-      })
-
-      vim.api.nvim_create_autocmd({ "WinScrolled", "BufWinEnter", "CursorHold", "InsertLeave", "BufModifiedSet", }, {
-        group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-        callback = function()
-        require("barbecue.ui").update()
-      end,
-      })
-    end,
-  },
-
+  --
+  -- {
+  --   "utilyre/barbecue.nvim",
+  --   name = "barbecue",
+  --   version = "*",
+  --   dependencies = {
+  --     "SmiteshP/nvim-navic",
+  --   },
+  --   event= "LspAttach",
+  --   config  = function ()
+  --     vim.opt.updatetime = 200
+  --
+  --     require("barbecue").setup({
+  --       create_autocmd = false,
+  --     })
+  --
+  --     vim.api.nvim_create_autocmd({ "WinScrolled", "BufWinEnter", "CursorHold", "InsertLeave", "BufModifiedSet", }, {
+  --       group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+  --       callback = function()
+  --       require("barbecue.ui").update()
+  --     end,
+  --     })
+  --   end,
+  -- },
+  --
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -3,14 +3,9 @@ return {
 	{
 		"utilyre/sentiment.nvim",
 		version = "*",
-		event = "VeryLazy", -- keep for lazy loading
+		event = "VeryLazy",
 		opts = {
-			-- config
 		},
-		init = function()
-			-- `matchparen.vim` needs to be disabled manually in case of lazy loading
-			vim.g.loaded_matchparen = 1
-		end,
 	},
 
 	{
