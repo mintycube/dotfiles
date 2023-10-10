de65851b0b06685bb74ff80f6178e6cae3d09f68
mintycube <90507714+osama-aslam@users.noreply.github.com>
2023-10-10 12:04:16 +0500

    Moved Lazy config into separate file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,74 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+require("manager")
 require("options")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-	defaults = { lazy = true },
-	spec = {
-		require("plugins.ui"),
-		require("plugins.treesitter"),
-		require("plugins.lspconfig"),
-		require("plugins.none-ls"),
-		require("plugins.completion"),
-		require("plugins.fzf"),
-		require("plugins.toggleterm"),
-		require("plugins.utils"),
-		require("plugins.alpha"),
-	},
-	ui = { border = "rounded" },
-	performance = {
-		cache = {
-			enabled = true,
-		},
-		reset_packpath = true,
-		rtp = {
-			disabled_plugins = {
-        "2html_plugin",
-        "tohtml",
-        "getscript",
-        "getscriptPlugin",
-        "gzip",
-        "logipat",
-        "netrw",
-        "netrwPlugin",
-        "netrwSettings",
-        "netrwFileHandlers",
-        "matchit",
-				"matchparen",
-        "tar",
-        "tarPlugin",
-        "rrhelper",
-        "spellfile_plugin",
-        "vimball",
-        "vimballPlugin",
-        "zip",
-        "zipPlugin",
-        "tutor",
-        "rplugin",
-        "syntax",
-        "synmenu",
-        "optwin",
-        "compiler",
-        "bugreport",
-        "ftplugin",
-			}
-		}
-	}
-})
-
 require("autocommands")
 require("mappings")
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -73,12 +73,4 @@ vim.opt.pumheight = 10
 
 if vim.g.neovide then
 	vim.o.guifont = "JetBrainsMono NF:h10"
-	vim.opt.linespace = 0
-	vim.g.neovide_scale_factor = 1.0
-	-- vim.g.neovide_floating_blur_amount_x = 2.0
-	-- vim.g.neovide_floating_blur_amount_y = 2.0
-	-- vim.g.neovide_transparency = 0.8
-	-- vim.g.neovide_hide_mouse_when_typing = false
-	-- vim.g.neovide_refresh_rate = 60
-	-- vim.g.neovide_refresh_rate_idle = 5
 end
