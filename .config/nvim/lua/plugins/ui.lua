c8a1c1a96ede2cefc8b4b0974958df9a7db94a96
mintycube <90507714+mintycube@users.noreply.github.com>
2024-05-20 16:12:22 +0500

    update to 0.10 and add transparency

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -44,6 +44,8 @@ return {
         end,
       })
       vim.cmd.colorscheme("tokyonight")
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   },
 
